38985884bb45da8ba819fb6a0a0ba56488a210ab
rustam023 <r.91@mail.ru>
2020-11-06 14:10:12 +0300

    add some info

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,72 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Rustam Shaidullov',
+  title: 'Ruby on Rails developer',
   facts: {
-    'Residence': 'London',
-    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-                  <span class="screen">wiki</span>
-                </a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
-  },
-  skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
-  ],
-  plays: [
-    {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
-      contents: `
+    'Residence': `<a href="https://www.google.com/maps/place/Геологический+музей+им.+А.+А.+Штукенберга/@55.7941959,49.1130785,16.75z/data=!4m5!3m4!1s0x415ead164ec8c7fd:0x96d9300f434ca5b5!8m2!3d55.7939324!4d49.1144389">Kazan</a>`,
+    'LinkedIn': `<a href="https://www.linkedin.com/in/rustam-shaidullov-2213226b/">rustam-shaidullov</a>`,
+    'GitHub': '<a href="https://github.com/rustam023">rust-am</a>',
+    'StackOverflow': '<a href="https://stackoverflow.com/users/story/8043097?_=1">rust-am</a>',
+    'HackerRank': '<a href="https://www.hackerrank.com/rust_am?hr_r=1">rust_am</a>',
+    'Email': '<a href="mailto:28rustam@gmail.com">28rustam@gmail.com</a>'
+},
+
+skills: [
+  ['Comedy', '++++'],
+  ['Love', '++++'],
+  ['Theater', '++++'],
+  ['Tragedy', '++++'],
+  ['Poetry', '++++'],
+  ['Erotic', '+++'],
+  ['Sonnets', '++++'],
+  ['Plays', '++++'],
+  ['Writing', '++++'],
+  ['Directing', '+++'],
+  ['Literature', '++++'],
+  ['Influence', '++++'],
+],
+  plays:
+[
+  {
+    title: 'Hamlet',
+    written: 'Likely early 17th century',
+    badges: ['tragedy', 'philosophy', 'death'],
+    contents: `
 Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
 `
-    },
-    {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
-      contents: `
+  },
+  {
+    title: 'Twelfth Night',
+    written: '1600 – 1601',
+    badges: ['comedy', 'love'],
+    contents: `
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
 `
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
+  },
+  {
+    title: 'The Tragedy of Romeo and Juliet',
+    written: '1595–1596, with a possible early draft written in 1591',
+    badges: ['tragedy', 'death', 'love'],
+    contents: `
 In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
 `
-    }
-  ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
+  }
+],
+  experience:
+[
+  {
+    header: 'Performance',
+    skills: ['theater', 'play'],
+    contents: `
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
 `
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
+  },
+  {
+    header: 'Poetry',
+    skills: ['poetry', 'erotic'],
+    contents: `
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
 `
-    }
-  ]
-};
+  }
+]
+}
+;
